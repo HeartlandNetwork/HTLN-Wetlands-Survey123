9e888853211561a7e322c9ffd949a5307666d74b
HeartlandNetwork <gareth_rowell@nps.gov>
2023-09-12 12:33:55 -0500

    woody var names

--- src/Survey123_VIBI_woody.R
+++ src/Survey123_VIBI_woody.R
@@ -1,6 +1,7 @@
 
-library(tidyverse)
 library(dplyr)
+library(readr)
+
 
 
 setwd("../HTLN-Wetlands-Survey123/src")
@@ -16,19 +17,38 @@ glimpse(load_file)
 # 
 
 Access_data <- load_file |>
-	select(WoodyModule, WoodySpecies, EditDate, WoodySiteName, ShrubClump, D0-1))
+	select(WoodyModule, WoodySpecies, EditDate, WoodySiteName, ShrubClump, D0to1,
+	       D1to2_5, D2_5to5, D5to10, D10to15, D15to20, D20to25, D25to30, D30to35,
+	       D35to40, Dgt40, Dgt40_1, Dgt40_2, Dgt40_3, Dgt40_4, Dgt40_5)
 
-D10-15, D15-20, D20-25, D25-30, D30-35, D35-40 ) |>
+Access_data <- Access_data |>
   mutate( FeatureID = WoodySiteName) |>
   mutate(EditDate = (EditDate <- as.Date(EditDate, format = "%m/%d/%Y")))
 
 glimpse(Access_data)
 
-# Substitute NA with -9999 in CoverClass and CoverClassAll
+# Substitute NA with -9999 in all dbl variable
 
+Access_data$ShrubClump <- Access_data$ShrubClump |> replace_na(-9999)
+Access_data$D0to1 <- Access_data$CoverD0to1 |> replace_na(-9999)
+Access_data$D1to2_5 <- Access_data$D1to2_5 |> replace_na(-9999)
+Access_data$D2_5to5 <- Access_data$D2_5to5 |> replace_na(-9999)
+Access_data$D5to10 <- Access_data$D5to10 |> replace_na(-9999)
+Access_data$D10to15 <- Access_data$15to20 |> replace_na(-9999)
+Access_data$D15to20 <- Access_data$CoverClass |> replace_na(-9999)
+Access_data$CoverClass <- Access_data$CoverClass |> replace_na(-9999)
+Access_data$CoverClass <- Access_data$CoverClass |> replace_na(-9999)
+Access_data$CoverClass <- Access_data$CoverClass |> replace_na(-9999)
+Access_data$CoverClass <- Access_data$CoverClass |> replace_na(-9999)
 Access_data$CoverClass <- Access_data$CoverClass |> replace_na(-9999)
 
-Access_data$CoverClassAll <- Access_data$CoverClassAll |> replace_na(-9999)
+
+# Generate EventID from EditDate
+
+Access_data <- Access_data |>
+	  mutate( EventID = str_c( 'CUVAWetlnd', EditDate)) |>
+	    mutate(EventID = str_replace_all(EventID, "-", ""))
+
 
 
 # Generate EventID from EditDate
